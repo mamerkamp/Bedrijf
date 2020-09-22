d2197e6ff79488b47538e28ce86325ba85b2d613
Mark Amerkamp <markamerkamp@iMac-van-Mark.local>
2020-09-22 14:26:24 +0200

    Opdracht 4.1 onderdeel 3

--- src/model/Afdeling.java
+++ src/model/Afdeling.java
@@ -33,4 +33,8 @@ public class Afdeling {
     public void setAfdelingsPlaats(String afdelingsPlaats) {
         this.afdelingsPlaats = afdelingsPlaats;
     }
+
+    public String toString(){
+        return "afdeling " + afdelingsNaam + " te " + afdelingsPlaats;
+    }
 }
