4e13a5e4dbff8ac03f2752ef98633fa980f8cd96
Mark Amerkamp <markamerkamp@iMac-van-Mark.local>
2020-09-22 10:32:32 +0200

    Verdere uitwerking opdracht 3.1

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -26,8 +26,12 @@ public class BedrijfLauncher {
         Persoon assistent = new Persoon("Klaas");
 
         System.out.println("Het aantal personen in het bedrijf is " + Persoon.aantalPersonen);
-        System.out.printf("%s werkt in %s en woont in %s\n", baas.getNaam(), baas.getAfdeling().getAfdelingsPlaats(), baas.getWoonplaats());
-        System.out.printf("%s werkt op de afdeling %s en verdient %.2f", medewerker.getNaam(), medewerker.getAfdeling(), medewerker.getMaandSalaris());
+        System.out.printf("%s werkt in %s en woont in %s\n", baas.getNaam(), baas.getAfdeling().getAfdelingsPlaats(),
+                baas.getWoonplaats());
+        System.out.printf("%s werkt op de afdeling %s en verdient %.2f\n", medewerker.getNaam(),
+                medewerker.getAfdeling().getAfdelingsNaam(), medewerker.getMaandSalaris());
+        System.out.printf("%s werkt op de afdeling %s en woont in %s", assistent.getNaam(),
+                assistent.getAfdeling().getAfdelingsNaam(), assistent.getWoonplaats());
 
 
     }
